8ee699eaa2f5ddcba54afb5777e59e233978d455
hejiawei <hejiawei@qiyi.com>
2020-04-30 17:19:32 +0800

    添加自定义动态数组NSMutableArray

--- src/com/SaiDiCaprio/Main.java
+++ src/com/SaiDiCaprio/Main.java
@@ -1,9 +1,21 @@
 package com.SaiDiCaprio;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        fibonacci(46);
+        testMutableArray();
+//        ArrayList
         System.out.println("Hello World!");
+    }
+
+    private static void testMutableArray() {
+        NSMutableArray<Person> array = new NSMutableArray<Person>();
+    }
+
+
+    private static void testFibonacci() {
+        fibonacci(46);
     }
 
     private static void fibonacci(int n) {
@@ -13,6 +25,7 @@ public class Main {
                 System.out.println(Fibonacci.fib1(n));
             }
         });
+
         TimeTool.check("fib2", new TimeTool.Task() {
             @Override
             public void execute() {
